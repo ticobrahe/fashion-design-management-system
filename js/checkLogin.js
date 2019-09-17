de58e9a025fd636f44b47e46c142080ab221acb1
ticobrahe <olabiyi.sam@gmail.com>
2019-09-17 21:04:58 +0100

    user can logout - implement logout feature - modified checklogin.js - made changes to signup.html - made changes to login.html

--- js/checkLogin.js
+++ js/checkLogin.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
   const user = localStorage.getItem("email");
-  if (user) {
+  if (!user) {
+    $("#logout").css("display", "none");
+  } else {
     $("#login").css("display", "none");
     $("#register").css("display", "none");
   }
+  $("#signout").click(function(e) {
+    e.preventDefault();
+    localStorage.removeItem("email");
+    window.location.assign("login.html");
+  });
 });
