fb8b33408aa1fb8c0fa80ec6c1e0ee20f00b3c5b
ticobrahe <olabiyi.sam@gmail.com>
2019-09-18 21:14:28 +0100

    Stylist can update a design - added updateDesign.js - implement update design feature

---
+++ js/updateDesign.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+  const getParam = new URLSearchParams(window.location.search);
+  const id = getParam.get("id");
+  $.getJSON(`http://localhost:3000/designs?id=${id}`, function(response) {
+    $.each(response, function(key, design) {
+      $("#styleName").val(`${design.styleName}`);
+      $("#description").val(`${design.description}`);
+      $("#price").val(`${design.price}`);
+      $("#image").val(`${design.imageUrl}`);
+      $("#update").click(function(e) {
+        e.preventDefault();
+        const styleName = $("#styleName").val() || design.styleName;
+        const description = $("#description").val() || design.description;
+        const price = $("#price").val() || design.price;
+        const imageUrl = $("#image").val() || design.imageUrl;
+        $.ajax({
+          method: "PUT",
+          url: `http://localhost:3000/designs/${id}`,
+          data: {
+            styleName,
+            description,
+            price,
+            imageUrl
+          },
+          success: function(response) {
+            $(".update-message").html("Design Updated succesfully");
+            window.location.assign(`design.html?id=${id}`);
+          }
+        });
+      });
+    });
+  });
+});
